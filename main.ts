5b971c4cdfb8416d110fd8e4536e3da697e4a0cc
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-11-03 13:41:05 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+// <<created by Vaughn Hatherall
+// Made on November 3rd 2020
+// Turns a servo motor that reveals a green led then uses sonar to light up neo pixels
+let strip: neopixel.Strip = null
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+basic.forever(function () {
+	
+})
